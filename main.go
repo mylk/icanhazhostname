136d8a42ae496b6774e5c75b5e6a9b4dd8dbd93f
Kostas Milonas <milonas.ko@gmail.com>
2022-02-10 01:24:39 +0200

    modifies name generate to return the description and links

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
     t, _ := template.ParseFiles("index.html")
     // executes the template, writing the generated HTML to the http.ResponseWriter
-    p2.Title = GetRandomName()
+    p2.Title = GetRandomName().Adjective + GetRandomName().Name
     t.Execute(w, p2)
 }
 
