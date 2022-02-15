2a0ebb178f605dbb816ed3d24803b4009ed70b2f
Kostas Milonas <milonas.ko@gmail.com>
2022-02-15 22:30:03 +0200

    creates a route to return a result in plain text

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "html/template"
     "log"
     "net/http"
@@ -20,6 +21,11 @@ func rootHandler(writer http.ResponseWriter, request *http.Request) {
     tmpl.ExecuteTemplate(writer, "base", &person)
 }
 
+func plainHandler(writer http.ResponseWriter, request *http.Request) {
+    person := GetRandomName()
+    fmt.Fprintf(writer, "%s%s", person.Adjective, person.Name)
+}
+
 func aboutHandler(writer http.ResponseWriter, request *http.Request) {
     tmpl := template.Must(template.New("about").ParseFiles("base.html", "about.html"))
     tmpl.ExecuteTemplate(writer, "base", nil)
@@ -27,7 +33,9 @@ func aboutHandler(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
     http.HandleFunc("/", rootHandler)
+    http.HandleFunc("/plain", plainHandler)
     http.HandleFunc("/about", aboutHandler)
+
     // ListenAndServe always returns an error, since it only returns when an unexpected error occurs.
     // in order to log that error we wrap the function call in log.Fatal.
     log.Fatal(http.ListenAndServe(":8080", nil))
